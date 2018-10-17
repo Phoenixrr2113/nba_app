7a1ce12852699c1a9d6f2322ef24726f22980765
Randy Wilson <phoenixrr2113@gmail.com>
2018-10-17 04:43:07 -0400

    first commit

---
+++ src/hoc/Layout/layout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './layout.css';
+
+class Layout extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        Header
+        <div>{this.props.children}</div>
+        Footer
+      </div>
+    );
+  }
+}
+
+export default Layout;
